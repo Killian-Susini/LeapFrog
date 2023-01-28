b5fa617fc7d081eb67e18a9f5cd943eab0c20cb2
Killian Susini <killian.susini@dauphine.eu>
2023-01-28 13:53:27 +0100

    OK for records with one element

--- LeapFrog/LeapFrog.cpp
+++ LeapFrog/LeapFrog.cpp
@@ -2,10 +2,102 @@
 //
 
 #include <iostream>
+#include <vector>
+#include <algorithm>
+#define BEGIN -1
+#define END 65536
+
+
+bool customComp(std::vector<int> a, std::vector<int> b)
+{ 
+    return a[1] > b[1]; 
+}
+
+std::vector<int> leapFrogJoin(std::vector<std::vector<int>> records)
+{
+    std::vector<int> new_record{ BEGIN };
+    int num_records = records.size();
+    std::vector<std::vector<int>::iterator> recordsIterators;
+
+    for (int record = 0; record < num_records; record++) {
+        recordsIterators.push_back(records[record].begin());
+    }
+
+
+    while (*recordsIterators.front() != END)
+    {
+        recordsIterators.front()++;
+        // sync
+        int max = *recordsIterators.front();
+        int min = *recordsIterators.back();
+        int p = 1;
+        while (max != min)
+        {
+            recordsIterators[p] = std::lower_bound(recordsIterators[p], records[p].end(), max);
+            max = *recordsIterators[p];
+            p = (p + 1) % num_records;
+            min = *recordsIterators[p];
+        }
+        new_record.push_back(*recordsIterators.front());
+    }
+    return new_record;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::cout << "Record 1: ";
+
+
+    std::vector<int> record1{ BEGIN, 2, 3, 5, 12, 15, 29, 30, 42, 43, END };
+    std::vector<int> record2{ BEGIN, 5, 6, 8, 12, 22, 25, 29, 32, 42, 45, END };
+    std::vector<int> record3{ BEGIN, 1, 2, 5, 9, 10, 29, 34, 35, 37, 42, END };
+
+    std::vector<std::vector<int>> records {record1, record2, record3};
+
+    int num = 1;
+    for (auto const& record : records) {
+
+        std::cout << "\nRecord " << num++ << " : ";
+        for (auto const& i : record) {
+            std::cout << i << " ";
+        }
+
+    }
+
+
+    std::cout << "\nSorting the records to get the first element to be greater.\n";
+
+    std::sort(records.begin(), records.end(), customComp);
+
+
+
+    num = 1;
+    for (auto const& record : records) {
+
+        std::cout << "\nRecord " << num++ << " : ";
+        for (auto const& i : record) {
+            std::cout << i << " ";
+        }
+
+    }
+
+
+    auto ptr = record1.begin();
+
+    std::cout << "\n" << *ptr << "\n";
+
+    ptr = std::lower_bound(ptr, record1.end(), 13);
+
+    std::cout << *ptr << "\n";
+
+
+
+    auto res = leapFrogJoin(records);
+    std::cout << "\n\n Result : ";
+    for (auto const& i : res) {
+        std::cout << i << " ";
+    }
+    std::cout << "\n";
 }
 
 // Exécuter le programme : Ctrl+F5 ou menu Déboguer > Exécuter sans débogage
